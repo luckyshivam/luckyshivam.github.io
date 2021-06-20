de6a2c054643a07adb28fcb36ba04c8b384e94a9
Shivam Dubey <technicalexpindia@gmail.com>
2021-06-20 12:49:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    $("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $("#image").attr('src',profile.getImageUrl());
+    $(".data").css("display","block");
+    $(".g-signin2").css("display","none")
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      alert("you have signed out sucessfully");
+      $(".g-signin2").css("display","block");
+      $(".data").css("display","none");
+
+    });
+  }
